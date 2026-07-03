0356850b3a6d1be11520c50bc9b87264a41e6821
kozyrev-dv <k-daniilv@mail.ru>
2026-07-03 18:10:14 +0300

    formatter seems to be set. vhdl sources formated

--- src/audio_process_top.vhd
+++ src/audio_process_top.vhd
@@ -9,99 +9,117 @@ entity audio_process_top is
         G_AUD_CLK   : integer := 12_228_000
     );
     port (
-        clk      : in    std_logic;
-        clk_aud    : in    std_logic; -- 12.288 MHz from external PLL
-        reset_n       : in    std_logic;
+        clk     : in    std_logic;
+        clk_aud : in    std_logic; -- 12.288 MHz from external PLL
+        reset_n : in    std_logic;
 
         -- I2C Interface (Connected to FPGA_I2C_SCL/SDA)
-        FPGA_I2C_SCL  : inout std_logic;
-        FPGA_I2C_SDA  : inout std_logic;
+        fpga_i2c_scl : inout std_logic;
+        fpga_i2c_sda : inout std_logic;
 
         -- Audio CODEC Interface (FPGA is Master)
-        AUD_XCK       : out   std_logic;
-        AUD_BCLK      : out   std_logic;
-        AUD_ADCLRCK   : out   std_logic;
-        AUD_ADCDAT    : in    std_logic;
-        AUD_DACLRCK   : out   std_logic;
-        AUD_DACDAT    : out   std_logic
+        aud_xck     : out   std_logic;
+        aud_bclk    : out   std_logic;
+        aud_adclrck : out   std_logic;
+        aud_adcdat  : in    std_logic;
+        aud_daclrck : out   std_logic;
+        aud_dacdat  : out   std_logic
     );
-end audio_process_top;
+end entity audio_process_top;
 
 architecture rtl of audio_process_top is
 
-    
-
-    component ssm2603_config
-        generic(
+    component ssm2603_config is
+        generic (
             G_INPUT_CLK   : integer := 50_000_000;
             G_DEVICE_ADDR : integer := 16#36#
         );
-        port(
-            clk             : in  std_logic;
-            rst_n           : in  std_logic;
-            ena_i           : in  std_logic;
-            busy_o          : out std_logic;
-            i2c_ena_o       : out std_logic;
-            i2c_addr_o      : out std_logic_vector (6 downto 0);
-            i2c_rw_o        : out std_logic;
-            i2c_data_wr_o   : out std_logic_vector(7 downto 0);
-            i2c_busy_i      : in  std_logic;
-            i2c_ack_error_i : in  std_logic
+        port (
+            clk             : in    std_logic;
+            rst_n           : in    std_logic;
+            ena_i           : in    std_logic;
+            busy_o          : out   std_logic;
+            i2c_ena_o       : out   std_logic;
+            i2c_addr_o      : out   std_logic_vector(6 downto 0);
+            i2c_rw_o        : out   std_logic;
+            i2c_data_wr_o   : out   std_logic_vector(7 downto 0);
+            i2c_busy_i      : in    std_logic;
+            i2c_ack_error_i : in    std_logic
         );
     end component ssm2603_config;
+
+    component i2c_master is
+        generic (
+            G_INPUT_CLK_HZ : integer := 50_000_000;
+            G_BUS_CLK_HZ   : integer := 400_000
+        );
+        port (
+            clk_i       : in    std_logic;
+            reset_n_i   : in    std_logic;
+            ena_i       : in    std_logic;
+            addr_i      : in    std_logic_vector(6 downto 0);
+            rw_i        : in    std_logic;
+            data_wr_i   : in    std_logic_vector(7 downto 0);
+            busy_o      : out   std_logic;
+            data_rd_o   : out   std_logic_vector(7 downto 0);
+            ack_error_o : buffer std_logic;
+            sda_io      : inout std_logic;
+            scl_io      : inout std_logic
+        );
+    end component i2c_master;
 
     signal reset_hard_n : std_logic;
 
     -- Audio CODEC signals
-    signal ena_aud_config     : std_logic;
-    signal busy_aud_config     : std_logic;
-    signal aud_config_i2c_ena     : std_logic := '0';
+    signal ena_aud_config         : std_logic;
+    signal busy_aud_config        : std_logic;
+    signal aud_config_i2c_ena     : std_logic                    := '0';
     signal aud_config_i2c_addr    : std_logic_vector(6 downto 0) := "0011011"; -- 0x1B (0x36 write address)
-    signal aud_config_i2c_rw      : std_logic := '0';
+    signal aud_config_i2c_rw      : std_logic                    := '0';
     signal aud_config_i2c_data_wr : std_logic_vector(7 downto 0) := (others => '0');
 
     -- I2C Master Control Signals
-    signal i2c_ena     : std_logic := '0';
-    signal i2c_addr    : std_logic_vector(6 downto 0) := "0011011"; -- 0x1B (0x36 write address)
-    signal i2c_rw      : std_logic := '0';
-    signal i2c_data_wr : std_logic_vector(7 downto 0) := (others => '0');
-    signal i2c_data_rd : std_logic_vector(7 downto 0) := (others => '0');
-    signal i2c_busy    : std_logic;
-    signal i2c_ack_error    : std_logic;
+    signal i2c_ena       : std_logic                    := '0';
+    signal i2c_addr      : std_logic_vector(6 downto 0) := "0011011"; -- 0x1B (0x36 write address)
+    signal i2c_rw        : std_logic                    := '0';
+    signal i2c_data_wr   : std_logic_vector(7 downto 0) := (others => '0');
+    signal i2c_data_rd   : std_logic_vector(7 downto 0) := (others => '0');
+    signal i2c_busy      : std_logic;
+    signal i2c_ack_error : std_logic;
 
-    signal i2c_data_rd_o : std_logic_vector(7 downto 0) := (others => '0');
-    signal i2c_busy_o    : std_logic;
-    signal i2c_ack_error_o    : std_logic;
+    signal i2c_data_rd_o   : std_logic_vector(7 downto 0) := (others => '0');
+    signal i2c_busy_o      : std_logic;
+    signal i2c_ack_error_o : std_logic;
 
-    
     -- codec config FSM States
-    type state_type is (INIT, AUD_CODEC_CONFIG, ACTIVE);
-    signal state : state_type := INIT;
+
+    type   state_type is (init, aud_codec_config, active);
+    signal state : state_type := init;
 begin
 
     -------------------------------------------------------------------
     -- 1. CLOCK GENERATION
     -------------------------------------------------------------------
-    AUD_XCK <= clk_aud;
+    aud_xck <= clk_aud;
 
     -------------------------------------------------------------------
     -- 2. DIGITAL AUDIO LOOPBACK
     -------------------------------------------------------------------
-    AUD_DACDAT <= AUD_ADCDAT; -- given every 
+    aud_dacdat <= aud_adcdat; -- given every
 
     -------------------------------------------------------------------
     -- 3. AUD CODEC CONFIGURATION CONTROLLER
     -------------------------------------------------------------------
-    ssm2603_config_inst : ssm2603_config
-        generic map(
-            G_INPUT_CLK => G_INPUT_CLK,
-            G_DEVICE_ADDR => 16#36#
+    ssm2603_config_inst : component ssm2603_config
+        generic map (
+            g_input_clk   => G_INPUT_CLK,
+            g_device_addr => 16#36#
         )
-        port map(
-            clk             => clk,
-            rst_n           => reset_hard_n,
-            ena_i           => ena_aud_config, -- block enabled; runs ones
-            busy_o          => busy_aud_config, -- busy configurating;
+        port map (
+            clk    => clk,
+            rst_n  => reset_hard_n,
+            ena_i  => ena_aud_config,
+            busy_o => busy_aud_config,
 
             i2c_ena_o       => aud_config_i2c_ena,
             i2c_addr_o      => aud_config_i2c_addr,
@@ -110,66 +128,71 @@ begin
             i2c_busy_i      => i2c_busy,
             i2c_ack_error_i => i2c_ack_error
         );
-    
-    
+
     -------------------------------------------------------------------
     -- 4. I2C MASTER INSTANTIATION
     -------------------------------------------------------------------
     i2c_mux : process (all) is
     begin
-        i2c_ena     <= '0';
-        i2c_addr    <= (others => '0');
-        i2c_rw      <= '0';
-        i2c_data_wr <= (others => '0');
-        i2c_data_rd <= (others => '0');
-        i2c_busy <= '0';
+        i2c_ena       <= '0';
+        i2c_addr      <= (others => '0');
+        i2c_rw        <= '0';
+        i2c_data_wr   <= (others => '0');
+        i2c_data_rd   <= (others => '0');
+        i2c_busy      <= '0';
         i2c_ack_error <= '0';
 
         case state is
-            when AUD_CODEC_CONFIG => 
-                i2c_ena     <= aud_config_i2c_ena;
-                i2c_addr    <= aud_config_i2c_addr;
-                i2c_rw      <= aud_config_i2c_rw;
-                i2c_data_wr <= aud_config_i2c_data_wr;
-                i2c_data_rd <= i2c_data_rd_o;
-                i2c_busy <= i2c_busy_o;
-                i2c_ack_error <= i2c_ack_error_o;    
+
+            when aud_codec_config =>
+                i2c_ena       <= aud_config_i2c_ena;
+                i2c_addr      <= aud_config_i2c_addr;
+                i2c_rw        <= aud_config_i2c_rw;
+                i2c_data_wr   <= aud_config_i2c_data_wr;
+                i2c_data_rd   <= i2c_data_rd_o;
+                i2c_busy      <= i2c_busy_o;
+                i2c_ack_error <= i2c_ack_error_o;
+
             when others =>
         end case;
     end process i2c_mux;
-    
+
     name : process (clk) is
     begin
         if rising_edge(clk) then
-            if reset_hard_n = '0' then
+            if (reset_hard_n = '0') then
                 ena_aud_config <= '0';
-                state <= INIT;
+                state          <= init;
             else
+
                 case state is
-                    when INIT => 
-                        state <= AUD_CODEC_CONFIG;
+
+                    when init =>
+                        state          <= aud_codec_config;
                         ena_aud_config <= '0';
-                    when AUD_CODEC_CONFIG =>
+
+                    when aud_codec_config =>
                         ena_aud_config <= '1';
                         if (ena_aud_config = '1' and busy_aud_config = '0') then
-                            state <= ACTIVE;
+                            state <= active;
                         end if;
-                    when ACTIVE =>
+
+                    when active =>
                         ena_aud_config <= '0';
-                        state <= ACTIVE;
-                    when others => 
+                        state          <= active;
+
+                    when others =>
                         ena_aud_config <= '0';
-                        state <= INIT;
+                        state          <= init;
                 end case;
             end if;
         end if;
     end process name;
-    
 
-    i2c_master_inst : i2c_master
+    i2c_master_inst : component i2c_master
         generic map (
-            G_INPUT_CLK => G_INPUT_CLK,
-            G_BUS_CLK   => G_AUD_CLK
+            g_input_clk_hz => G_INPUT_CLK,
+            g_bus_clk_hz   => G_AUD_CLK
         )
         port map (
             clk_i       => clk,
@@ -181,10 +204,9 @@ begin
             busy_o      => i2c_busy_o,
             data_rd_o   => i2c_data_rd_o,
             ack_error_o => i2c_ack_error_o,
-            sda_io       => FPGA_I2C_SDA,
-            scl_io       => FPGA_I2C_SCL
+            sda_io      => fpga_i2c_sda,
+            scl_io      => fpga_i2c_scl
         );
 
     reset_hard_n <= reset_n and not(i2c_ack_error);
-
-end rtl;
+end architecture rtl;
--- src/ssm2603_config.vhd
+++ src/ssm2603_config.vhd
@@ -5,29 +5,31 @@ library ieee;
 
 entity ssm2603_config is
     generic (
-        G_INPUT_CLK : integer := 50_000_000;
+        G_INPUT_CLK   : integer := 50_000_000;
         G_DEVICE_ADDR : integer := 16#36#
     );
-    port(
-        clk : in std_logic;
-        rst_n : in std_logic;
-        ena_i : in std_logic;
-        busy_o : out std_logic;
+    port (
+        clk    : in    std_logic;
+        rst_n  : in    std_logic;
+        ena_i  : in    std_logic;
+        busy_o : out   std_logic;
 
-        i2c_ena_o       : out std_logic;
-        i2c_addr_o      : out std_logic_vector (6 downto 0);
-        i2c_rw_o        : out std_logic;
-        i2c_data_wr_o   : out std_logic_vector(7 downto 0);
-        i2c_busy_i      : in std_logic;
-        i2c_ack_error_i : in std_logic
+        i2c_ena_o       : out   std_logic;
+        i2c_addr_o      : out   std_logic_vector(6 downto 0);
+        i2c_rw_o        : out   std_logic;
+        i2c_data_wr_o   : out   std_logic_vector(7 downto 0);
+        i2c_busy_i      : in    std_logic;
+        i2c_ack_error_i : in    std_logic
     );
 end entity ssm2603_config;
 
 architecture rtl of ssm2603_config is
+
     -- I2C Configuration ROM (16 bits: 7-bit addr + 9-bit data)
-    
-    type config_rom_type is array (0 to 7) of std_logic_vector(15 downto 0);
-    constant CONFIG_ROM : config_rom_type := (
+
+    type     config_rom_type is array (0 to 7) of std_logic_vector(15 downto 0);
+    constant CONFIG_ROM : config_rom_type :=
+    (
         x"1E00", -- R15: Reset
         x"0C30", -- R6:  Power up ADC/DAC/MIC, keep OUT off
         x"0815", -- R4:  Mic in, Boost on, Unmute, DAC select
@@ -39,97 +41,100 @@ architecture rtl of ssm2603_config is
     );
 
     -- codec config FSM States
-    type state_type is (INIT, TX_ADDR, TX_REG, TX_DATA, TX_DONE, DELAY, COMPLETE);
-    signal state      : state_type := INIT;
-    signal rom_index  : integer range 0 to 7 := 0;
-    signal delay_cnt  : integer range 0 to G_INPUT_CLK / 1000 := 0;
-    signal i2c_busy_prev : std_logic;
 
+    type   state_type is (init, tx_addr, tx_reg, tx_data, tx_done, delay, complete);
+    signal state         : state_type                            := init;
+    signal rom_index     : integer range 0 to 7                  := 0;
+    signal delay_cnt     : integer range 0 to G_INPUT_CLK / 1000 := 0;
+    signal i2c_busy_prev : std_logic;
 begin
-    
+
     -------------------------------------------------------------------
     -- 4. I2C WRAPPER STATE MACHINE (2-Byte Transmission)
     -------------------------------------------------------------------
-    process(clk)
+    command_sender_fsm : process (clk) is
     begin
         if rising_edge(clk) then
-            if rst_n = '0' then
-                i2c_ena_o <= '0';
-                state <= INIT;
+            if (rst_n = '0') then
+                i2c_ena_o     <= '0';
+                state         <= init;
                 i2c_data_wr_o <= (others => '0');
-                rom_index <= 0;
-                delay_cnt <= 0;
+                rom_index     <= 0;
+                delay_cnt     <= 0;
             elsif (ena_i = '1' and i2c_ack_error_i = '0') then
+
                 case state is
-                    when INIT =>
+
+                    when init =>
                         i2c_ena_o <= '0';
-                        state <= TX_ADDR;
-                        
-                    when TX_ADDR =>
-                        i2c_ena_o <= '1';
+                        state     <= tx_addr;
+
+                    when tx_addr =>
+                        i2c_ena_o     <= '1';
                         i2c_data_wr_o <= CONFIG_ROM(rom_index)(15 downto 8);
                         if (i2c_busy_prev = '1' and i2c_busy_i = '0') then
-                            state <= TX_REG;
-                        end if;
-                        
-                    when TX_REG =>
-                        if (i2c_busy_prev = '1' and i2c_busy_i = '0') then
-                            i2c_data_wr_o <= CONFIG_ROM(rom_index)(7 downto 0);
-                            state <= TX_DATA;
-                        else
-                            i2c_data_wr_o <= CONFIG_ROM(rom_index)(15 downto 8);
-                            state <= TX_REG;
+                            state <= tx_reg;
                         end if;
 
-                    when TX_DATA =>
+                    when tx_reg =>
+                        if (i2c_busy_prev = '1' and i2c_busy_i = '0') then
+                            i2c_data_wr_o <= CONFIG_ROM(rom_index)(7 downto 0);
+                            state         <= tx_data;
+                        else
+                            i2c_data_wr_o <= CONFIG_ROM(rom_index)(15 downto 8);
+                            state         <= tx_reg;
+                        end if;
+
+                    when tx_data =>
                         i2c_data_wr_o <= CONFIG_ROM(rom_index)(7 downto 0);
                         if (i2c_busy_prev = '1' and i2c_busy_i = '0') then
                             i2c_ena_o <= '0';
-                            state <= TX_DONE;
+                            state     <= tx_done;
                         end if;
 
-                    when TX_DONE =>
-                        if i2c_busy_i = '0' then
-                            if rom_index < CONFIG_ROM'length - 1 then
+                    when tx_done =>
+                        if (i2c_busy_i = '0') then
+                            if (rom_index < CONFIG_ROM'length - 1) then
                                 rom_index <= rom_index + 1;
-                                state <= DELAY;
+                                state     <= delay;
                             else
-                                state <= COMPLETE; -- Configuration complete
+                                state <= complete; -- Configuration complete
                             end if;
                         end if;
 
-                    when DELAY =>
+                    when delay =>
                         -- Brief pause between I2C configurations
-                        if delay_cnt = G_INPUT_CLK / 1000 then 
+                        if (delay_cnt = G_INPUT_CLK / 1000) then
                             delay_cnt <= 0;
-                            state <= TX_ADDR;
+                            state     <= tx_addr;
                         else
                             delay_cnt <= delay_cnt + 1;
                         end if;
-                        
-                    when COMPLETE =>
-                        state <= COMPLETE;
+
+                    when complete =>
+                        state     <= complete;
                         i2c_ena_o <= '0';
 
                     when others =>
-                        state <= INIT;
+                        state     <= init;
                         i2c_ena_o <= '0';
                 end case;
             end if;
         end if;
-    end process;
+    end process command_sender_fsm;
 
-    process (clk) begin
+    i2c_busy_gen : process (clk) is
+    begin
         if (rising_edge(clk)) then
-            if rst_n = '0' then
+            if (rst_n = '0') then
                 i2c_busy_prev <= '0';
             else
                 i2c_busy_prev <= i2c_busy_i;
             end if;
         end if;
-    end process;
-
-    busy_o <= '1' when (ena_i = '1') and (state /= COMPLETE) else '0'; 
+    end process i2c_busy_gen;
+    busy_o     <= '1' when (ena_i = '1') and (state /= complete) else
+                  '0';
     i2c_addr_o <= std_logic_vector(to_unsigned(G_DEVICE_ADDR, i2c_addr_o'length));
-    i2c_rw_o <= '0';
+    i2c_rw_o   <= '0';
 end architecture rtl;
